3390cb5cefbe18c22c5dda55b6938670500d8a79
MrElectric01/Christian+Conato <christianconato123@gmail.com>
2023-11-15 10:00:05 +0100

    Aggiunta della classe christian

---
+++ Prova/src/prova/ChriClass.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prova;
+
+/**
+ *
+ * @author chris
+ */
+public class ChriClass {
+    public void prova(){
+        System.out.println("Classe di christian");
+    }
+}
