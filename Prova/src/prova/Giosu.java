f9aa7726d656bc82ca9b524680dac4df6a005aed
GiosuCiaravola <giosueciaravola@gmail.com>
2023-11-15 10:00:08 +0100

    aggiunta classe Giosu

---
+++ Prova/src/prova/Giosu.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prova;
+
+/**
+ *
+ * @author giosu
+ */
+public class Giosu {
+    private String bestemmia;
+
+    public Giosu(String bestemmia) {
+        this.bestemmia = bestemmia;
+    }
+}
